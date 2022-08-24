5f86e3150d0efa589f3a4663c00b6fa0bc29a057
RhinolamerSonwar <rhinolamer@gmail.com>
2022-08-24 12:53:53 +0300

    modified config

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 // let sayHello = require("./sayHello");
 // sayHello("Lameck", document.querySelector("h2"))clg
-console.log("Hello LAmeck");
+// console.log("Hello LAmeck");
+import component from "./component";
+
+document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -7,4 +7,11 @@ module.exports = {
       title: "Webpack Output",
     }),
   ],
+  entry: {
+    main: path.resolve(__dirname, "./src/index.js"),
+  },
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "deploy"),
+  },
 };
