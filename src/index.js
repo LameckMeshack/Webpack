1a331628f3b29783caf8f1788a7ff0d09b5c1e0c
RhinolamerSonwar <rhinolamer@gmail.com>
2022-08-25 10:43:44 +0300

    configured karma

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 // let sayHello = require("./sayHello");
 // sayHello("Lameck", document.querySelector("h2"))clg
 // console.log("Hello LAmeck");
-import "./lameck.scss";
-import component from "./component";
-import "./image-component";
+// import "./lameck.scss";
+// import component from "./component";
+// import "./image-component";
+// import "./style.css";
+// document.body.appendChild(component());
 
-document.body.appendChild(component());
+// export default () => 10;
+function multiply(a, b) {
+  return a * b;
+}
---
+++ tests/index.spec.js
@@ -0,0 +1,13 @@
+// import myModule from "./../src/index.js";
+
+// describe("Module should return", function () {
+//   it("some number", function () {
+//     expect(myModule()).toEqual(10);
+//   });
+// });
+// console.log("trying the test");
+describe("Multiply", function () {
+  it("should return a product", function () {
+    expect(multiply(2, 3)).toEqual(6);
+  });
+});
